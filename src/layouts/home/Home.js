651b1118503e7dc45ae2d94d84bccd84d07c2d1e
Piyush Chakarborthy <102757301+Chakravartinsamrat@users.noreply.github.com>
2024-12-02 11:46:15 +0530

    Commit

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -6,9 +6,9 @@ class Home extends Component {
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>SVNIT DOTSLASH Hackathon</h1>
+            <h1>UNFOLD 2024 Hackathon</h1>
             <h2>Real Estate Mapping and Asset Tokenization</h2>
-            <img className="image" src="https://cdn-images-1.medium.com/max/1600/1*TKdUJZ3JFMBpm3zmPbm9mQ.png"  height="400" width="950" />
+            <img className="image" src="https://cdn-images-1.medium.com/max/1600/1*TKdUJZ3JFMBpm3zmPbm9mQ.png"  height="400" width="950" alt="rubbish" />
           </div>
         </div>
       </main>
